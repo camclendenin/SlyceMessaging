22b0f16dcfb50b409361ed8712c5bd844c9cde1d
Sam Bains <sam@g33k.limited>
2016-12-05 10:50:04 +0000

    Changes the failure message from a Toast to a SnackBar.

--- slyce-messaging/src/main/java/it/slyce/messaging/SlyceMessagingFragment.java
+++ slyce-messaging/src/main/java/it/slyce/messaging/SlyceMessagingFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -403,7 +404,7 @@ public class SlyceMessagingFragment extends Fragment implements OnClickListener 
             addNewMessage(textMessage, false);
             ScrollUtils.scrollToBottomAfterDelay(recyclerView, recyclerAdapter);
         } else {
-            Toast.makeText(getContext(), failureMessage, Toast.LENGTH_SHORT).show();
+            Snackbar.make(recyclerView, failureMessage, Snackbar.LENGTH_SHORT).show();
         }
     }
 
